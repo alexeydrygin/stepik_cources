f5dab0dc312418e8a776ce7ba7a83955cd577d7f
drygin_alexey <drygin.alex@mail.ru>
2023-03-30 17:16:09 +0700

    stepik

--- create_step_md.py
+++ create_step_md.py
@@ -2,7 +2,7 @@ N = int(input("Введите число N: "))  # получаем число N
 
 # создаем файлы step_1.md, step_2.md, и т.д., до step_N.md
 for i in range(1, N+1):
-    filename = f"step_{i}.md"
+    filename = f"{i}.md"
     with open(filename, "w") as f:
         f.write(f"## ")
 
