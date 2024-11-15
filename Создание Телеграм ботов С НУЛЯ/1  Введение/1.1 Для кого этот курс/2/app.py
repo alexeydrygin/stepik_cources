aa9b52e0fa37d0537b800a5397cf5344d875f0ba
drygin_alexey <drygin.alex@mail.ru>
2024-11-15 23:54:52 +0700

    введение

---
+++ Создание Телеграм ботов С НУЛЯ/1  Введение/1.1 Для кого этот курс/2/app.py
@@ -0,0 +1,18 @@
+from tkinter import ttk
+import tkinter as tk
+import time
+
+root = tk.Tk()
+root.geometry('300x300+150+150')
+
+def sleep_func():
+    time.sleep(10)
+    lab['text'] = 'После сна'
+
+btn = ttk.Button(root, text='Run', command=sleep_func)
+btn.place(relx=0.5, rely=0.2, anchor=tk.CENTER)
+
+lab = ttk.Label(root, text='Текст до нажатия')
+lab.place(relx=0.5, rely=0.6, anchor=tk.CENTER)
+
+root.mainloop()
